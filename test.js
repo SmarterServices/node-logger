3c2fab02bcd76a7d62a64f5aa28c5bb6a7bcb606
Badiul Haque Shawon <badiul.haque@enosisbd.com>
2017-10-25 17:17:04 +0600

    #151517616 - Add a test file for demonstration purpose

---
+++ test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Logger = require('./index');
+
+const logger = new Logger({
+  'targets': [
+    {
+      'name': 'console'
+    },
+    {
+      'name': 'file',
+      'options': {
+        'directory': 'logs',
+        'sync': true
+      }
+    }
+  ]
+});
+
+logger.log('Hello', 'World!');
+logger.info('Testing', 'logger');
+logger.warn('Warning');
+logger.error('Danger! Your App is crashed!');
+
+logger.with('console').log('This will only be printed in console');
+logger.exclude('file').log('This will not be printed in file');
+logger.exec(console.error, 'Print an error using given function');
